47e6d91b1111d6dd49a86942f03fcdd75d177527
JaAncaten <ja.ancaten@duocuc.cl>
2026-06-29 22:28:47 -0400

    Se agregraon microservicios

--- src/main/java/com/perfulandia/gatewayservice/config/GatewayConfig.java
+++ src/main/java/com/perfulandia/gatewayservice/config/GatewayConfig.java
@@ -11,11 +11,40 @@ public class GatewayConfig {
     @Bean
     public RouteLocator rutas(RouteLocatorBuilder builder) {
         return builder.routes()
-
+                // SOPORTE SERVICE
                .route("soporte-service", r -> r
                     .path("/api/soporte/**")
                     .filters(f -> f.rewritePath("/api/soporte/(?<segment>.*)", "/api/${segment}"))
                     .uri("http://localhost:8070"))
+
+
+
+                // PEDIDO SERVICE
+                .route("pedido-service", r -> r
+                    .path("/api/pedidos", "/api/pedidos/**")
+                    .uri("http://localhost:8071"))
+
+                // TIENDAS SERVICE
+                .route("tiendas-service", r -> r
+                        .path("/api/v1/tiendas", "/api/v1/tiendas/**")
+                        .uri("http://localhost:8091"))
+
+
+
+                // INVENTARIO / CATALOGO SERVICE
+                .route("inventario-catalogo-service", r -> r
+                        .path(
+                                "/api/v1/productos", "/api/v1/productos/**",
+                                "/api/v1/categorias", "/api/v1/categorias/**",
+                                "/api/v1/inventarios", "/api/v1/inventarios/**"
+                        )
+                        .uri("http://localhost:8092"))
+
+
+                // RESEÑA SERVICE
+                .route("resena-service", r -> r
+                        .path("/api/v1/resenas", "/api/v1/resenas/**")
+                        .uri("http://localhost:8093"))
 
                 .build();
     }
