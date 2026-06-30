ccb798f256dc904cb8c2b77c9342dc2efc02687c
JaAncaten <ja.ancaten@duocuc.cl>
2026-06-29 22:37:07 -0400

    Se agregraon mas pruebas unitarias a  el ServiceAplicationTest

--- src/test/java/com/perfulandia/gatewayservice/GatewayServiceApplicationTests.java
+++ src/test/java/com/perfulandia/gatewayservice/GatewayServiceApplicationTests.java
@@ -3,10 +3,22 @@ package com.perfulandia.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest(properties = "server.port=0")
 class GatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        // Contexto cargado correctamente
+    }
+
+    @Test
+    void main_deberiaIniciarLaAplicacionSinLanzarExcepcion() {
+        assertDoesNotThrow(() ->
+                GatewayServiceApplication.main(new String[] {
+                        "--server.port=0"
+                })
+        );
     }
 }
